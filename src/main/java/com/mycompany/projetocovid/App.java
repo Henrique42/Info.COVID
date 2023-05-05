deaa4ec957e20b960912a084246b410d7dafb10f
Bruno Henrique de Jesus Silva <39635299+Henrique42@users.noreply.github.com>
2023-05-05 12:00:33 -0300

    App.java atualizado

--- src/main/java/com/mycompany/projetocovid/App.java
+++ src/main/java/com/mycompany/projetocovid/App.java
@@ -28,9 +28,11 @@ public class App extends Application {
     
     // Método que carrega o fxml
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+    
     
     // Método main, que inicia a aplicação
     public static void main(String[] args) {
